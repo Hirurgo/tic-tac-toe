b4657f788b9713a705339f0feac4aef1b2b9de64
Hirurgo <Keep_ya_head_up@inbox.ru>
2017-02-04 16:56:11 +0300

    Update class tictactoe

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,41 @@
 class TicTacToe {
-    constructor() {
+	constructor() {
+		this.currentPlayerSymbol = 'x';
+		this.matrix = [[null,null,null],[null,null,null],[null,null,null]];
+	}
 
-    }
+	getCurrentPlayerSymbol() {
+		return this.currentPlayerSymbol;
+	}
 
-    getCurrentPlayerSymbol() {
+	nextTurn(rowIndex, columnIndex) {
+		if (this.matrix[rowIndex][columnIndex] === null) {
+			this.matrix[rowIndex][columnIndex] =  this.currentPlayerSymbol;
+			if (this.currentPlayerSymbol === 'o')  this.currentPlayerSymbol = 'x';
+			else this.currentPlayerSymbol = 'o';
+		}
+	}
 
-    }
+	isFinished() {}
 
-    nextTurn(rowIndex, columnIndex) {
+	getWinner() {}
 
-    }
+	noMoreTurns() {
+		var w,q=3;
+		while(q--) {
+			w=3;
+			while(w--) {
+				if (this.matrix[q][w] === null) return false;
+			}
+		}
+		return true;
+	}
 
-    isFinished() {
+	isDraw() {}
 
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+	getFieldValue(rowIndex, colIndex) {
+		return this.matrix[rowIndex][colIndex];
+	}
 }
 
 module.exports = TicTacToe;
