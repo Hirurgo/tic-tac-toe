7d87498eabf94b55a040a9b1c317dfac69d86ab8
Hirurgo <Keep_ya_head_up@inbox.ru>
2017-02-05 00:25:33 +0300

    nothing

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -18,7 +18,46 @@ class TicTacToe {
 
 	isFinished() {}
 
-	getWinner() {}
+	getWinner() {
+		// var i,w,q,count,Draw;
+		// var symbol=['x','o'];
+
+		// i=2;
+		// while(i--) {
+		// 	Draw=0;
+		// 	q=3;
+		// 	while(q--) {
+		// 		w=3;
+		// 		count=0;
+		// 		while(w--) {
+		// 			if (this.matrix[q][w] === symbol[i]) count++;
+		// 		}
+		// 		if (count>2) return symbol[i];
+		// 	}
+
+		// 	q=3;
+		// 	while(q--) {
+		// 		w=3;
+		// 		count=0;
+		// 		while(w--) {
+		// 			if (this.matrix[w][q] === symbol[i]) count++;
+		// 		}
+		// 		if (count>2) return symbol[i];
+		// 	}
+
+		// 	count=0
+		// 	if (this.matrix[0][0] === symbol[i]) count++;
+		// 	if (this.matrix[1][1] === symbol[i]) count++;
+		// 	if (this.matrix[2][1] === symbol[i]) count++;	
+		// 	if (count>2) return symbol[i];
+
+		// 	if (this.matrix[0][2] === symbol[i]) count++;
+		// 	if (this.matrix[1][1] === symbol[i]) count++;
+		// 	if (this.matrix[2][0] === symbol[i]) count++;	
+		// 	if (count>2) return symbol[i];
+		// }
+		// return null;
+	}
 
 	noMoreTurns() {
 		var w,q=3;
@@ -31,7 +70,9 @@ class TicTacToe {
 		return true;
 	}
 
-	isDraw() {}
+	isDraw() {
+		
+	}
 
 	getFieldValue(rowIndex, colIndex) {
 		return this.matrix[rowIndex][colIndex];
