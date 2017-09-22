6f8c603a53b3a5a11d2d1921d5965dcc0c437c0f
Hirurgo <Keep_ya_head_up@inbox.ru>
2017-09-22 21:23:30 +0300

    refactoring

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,46 +1,23 @@
 module.exports = class TicTacToe {
-	constructor() {
-		this.player = 'x'
-		this.matrix = Array(9).fill(null)
-		this.genIndex = (row, col) => 3*row + col
-		this.winSets = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-	}
-
-	getCurrentPlayerSymbol() {
-		return this.player;
-	}
-	
-	getFieldValue(row, col) {
-		return this.matrix[this.genIndex(row, col)]
-	}
-
-	nextTurn(row, col) {
-		let index = this.genIndex(row, col)
-		if (!this.matrix[index]) {
-			this.matrix[index] = this.player
-			this.player = this.player == 'x' ? 'o' : 'x'
+  constructor() {
+    let player = 'x', matrix = Array(9).fill(null)
+		const winSets = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],
+		genIndex = (row, col) => 3*row + col,
+    notEqualPrev = (e, i , arr) => e === arr[i-1] ? false : true,
+    toOnlyFirst = (first, current, i) => i ? null : first,
+    toFirst = (acc, current, i) => !i ? current : acc,
+    checkWin = cells => cells.map(this.getFieldValue).filter(notEqualPrev).reduce(toOnlyFirst)
+    this.getCurrentPlayerSymbol = () => player
+		this.getFieldValue = (row, col, arr) => matrix[arr ? row : genIndex(row, col)]
+		this.noMoreTurns = () => !matrix.filter(e=>e==null).length
+    this.getWinner = () =>  winSets.map(checkWin).filter(e=>e!=null).reduce(toFirst, null)
+		this.isDraw = () => this.noMoreTurns() && !this.getWinner() ? true : false
+		this.isFinished = () => this.isDraw() || this.getWinner() ? true : false
+		this.nextTurn = (row, col) => {
+			if (!matrix[genIndex(row, col)]) {
+				matrix[genIndex(row, col)] = player
+				player = player == 'x' ? 'o' : 'x'
+			}
 		}
-	}
-
-	isFinished() {
-		return this.isDraw() || this.getWinner() ? true : false;
-	}
-	
-	getWinner() {
-		const notNull = element => element !== null
-		const notEqualPrev = (element, i , arr) => element === arr[i-1] ? false : true
-		const takeValue = i => this.matrix[i]
-		const toOnlyFirst = (first, current, i) => i ? null : first
-		const toFirst = (acc, current, i) => !i ? current : acc
-		const checkWin = cells => cells.map(takeValue).filter(notEqualPrev).reduce(toOnlyFirst)
-		return this.winSets.map(checkWin).filter(notNull).reduce(toFirst, null);
-	}
-
-	noMoreTurns() {
-		return !this.matrix.filter(e => e === null).length
-	}
-
-	isDraw() {
-		return this.noMoreTurns() && !this.getWinner() ? true : false
 	}
 }
