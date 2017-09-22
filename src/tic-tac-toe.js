e8187eeac31e3e3fb2d04ea7c3e2392a01adbcaa
Hirurgo <Keep_ya_head_up@inbox.ru>
2017-09-22 20:36:36 +0300

    refactoring

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,24 +1,46 @@
-class TicTacToe {
+module.exports = class TicTacToe {
 	constructor() {
+		this.player = 'x'
+		this.matrix = Array(9).fill(null)
+		this.genIndex = (row, col) => 3*row + col
+		this.winSets = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 	}
 
 	getCurrentPlayerSymbol() {
+		return this.player;
+	}
+	
+	getFieldValue(row, col) {
+		return this.matrix[this.genIndex(row, col)]
 	}
 
-	nextTurn(rowIndex, columnIndex) {
+	nextTurn(row, col) {
+		let index = this.genIndex(row, col)
+		if (!this.matrix[index]) {
+			this.matrix[index] = this.player
+			this.player = this.player == 'x' ? 'o' : 'x'
+		}
 	}
 
 	isFinished() {
+		return this.isDraw() || this.getWinner() ? true : false;
 	}
-
+	
 	getWinner() {
+		const notNull = element => element !== null
+		const notEqualPrev = (element, i , arr) => element === arr[i-1] ? false : true
+		const takeValue = i => this.matrix[i]
+		const toOnlyFirst = (first, current, i) => i ? null : first
+		const toFirst = (acc, current, i) => !i ? current : acc
+		const checkWin = cells => cells.map(takeValue).filter(notEqualPrev).reduce(toOnlyFirst)
+		return this.winSets.map(checkWin).filter(notNull).reduce(toFirst, null);
 	}
 
 	noMoreTurns() {
+		return !this.matrix.filter(e => e === null).length
 	}
 
 	isDraw() {
+		return this.noMoreTurns() && !this.getWinner() ? true : false
 	}
 }
-
-module.exports = TicTacToe;
