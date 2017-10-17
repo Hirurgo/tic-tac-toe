09679eeda22c1816d99266039a0a9e70b1eaea34
Mikalai Shchakatsikhin <keep_ya_head_up@inbox.ru>
2017-10-17 12:53:34 +0300

    change player symbols

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,6 +1,6 @@
 module.exports = class TicTacToe {
   constructor() {
-    let player = 'X', field = Array(9).fill(null)
+    let player = 'x', field = Array(9).fill(null)
     const winSets = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],
     index = (row, col) => 3*row + col,
     checkWin = set => set.map(this.getFieldValue).join('').match(/(.)\1{2}/)
@@ -11,6 +11,6 @@ module.exports = class TicTacToe {
     this.isDraw = () => this.noMoreTurns() && !this.getWinner() 
     this.isFinished = () => this.isDraw() || !!this.getWinner()
     this.nextTurn = (row, col) => !field[index(row, col)] ? 
-      (field[index(row, col)] = player, player = player == 'X' ? 'O' : 'X') : null
+      (field[index(row, col)] = player, player = player == 'x' ? 'o' : 'x') : null
   }
 }
